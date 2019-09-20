688c23dbe62efc64f56d08ab15012920ebaf64f2
Karl Essinger <xkaess22@gmail.com>
2019-09-20 14:53:38 +0200

    Updated status command

--- SupportBoi/Commands/StatusCommand.cs
+++ SupportBoi/Commands/StatusCommand.cs
@@ -29,21 +29,32 @@ namespace SupportBoi.Commands
 					return;
 				}
 
-				bool isTicket = Database.IsTicket(command.Channel.Id);
-
 				long openTickets = Database.GetNumberOfTickets();
 				long closedTickets = Database.GetNumberOfClosedTickets();
 
-				DiscordEmbed message = new DiscordEmbedBuilder()
+				DiscordEmbed botInfo = new DiscordEmbedBuilder()
+					.WithAuthor("SupportBoi", "https://github.com/KarlofDuty/SupportBoi", "https://karlofduty.com/img/tardisIcon.jpg")
 					.WithTitle("Bot information")
 					.WithColor(DiscordColor.Cyan)
 					.AddField("Version", SupportBoi.GetVersion(), false)
-					.AddField("Repository", "https://github.com/KarlofDuty/SupportBoi", false)
 					.AddField("Open tickets", openTickets + "", true)
-					.AddField("Closed tickets (1.1.0+ tickets only)", closedTickets + " ", true)
-					.AddField("Channel info", isTicket ? "This channel is a ticket. (More info will be added here in a later version)" : "This channel is not a ticket.", false)
-					.WithFooter("KarlofDuty", "https://karlofduty.com/img/tardisIcon.jpg");
-				await command.RespondAsync("", false, message);
+					.AddField("Closed tickets (1.1.0+ tickets only)", closedTickets + " ", true);
+				await command.RespondAsync("", false, botInfo);
+
+				if (Database.TryGetTicket(command.Channel.Id, out Database.Ticket ticket))
+				{
+					DiscordEmbed channelInfo = new DiscordEmbedBuilder()
+						.WithAuthor("SupportBoi", "https://github.com/KarlofDuty/SupportBoi", "https://karlofduty.com/img/tardisIcon.jpg")
+						.WithTitle("Channel information")
+						.WithColor(DiscordColor.Cyan)
+						.AddField("Channel info", "This channel is a ticket.", false)
+						.AddField("Ticket number:", ticket.id.ToString(), true)
+						.AddField("Ticket creator:", $"<@{ticket.creatorID}>", true)
+						.AddField("Assigned staff:", ticket.assignedStaffID == 0 ? "Unassigned." : $"<@{ticket.assignedStaffID}>", true)
+						.AddField("Creation time:", ticket.createdTime.ToString(Config.timestampFormat), true)
+						.AddField("Summary:", string.IsNullOrEmpty(ticket.summary) ? "No summary." : ticket.summary, false);
+					await command.RespondAsync("", false, channelInfo);
+				}
 			}
 		}
 	}
