14b0ed37cb3c7b072c5c2f2f8efd12e9c3182617
Karl Essinger <xkaess22@gmail.com>
2019-10-12 20:47:54 +0200

    Indentation changes

--- SupportBoi/Commands/SetSummaryCommand.cs
+++ SupportBoi/Commands/SetSummaryCommand.cs
@@ -14,42 +14,44 @@ namespace SupportBoi.Commands
 		[Cooldown(1, 5, CooldownBucketType.User)]
 		public async Task OnExecute(CommandContext command)
 		{
+
+			// Check if the user has permission to use this command.
+			if (!Config.HasPermission(command.Member, "setsummary"))
+			{
+				DiscordEmbed error = new DiscordEmbedBuilder
+				{
+					Color = DiscordColor.Red,
+					Description = "You do not have permission to use this command."
+				};
+				await command.RespondAsync("", false, error);
+				command.Client.DebugLogger.LogMessage(LogLevel.Info, "SupportBoi", "User tried to use the setsummary command but did not have permission.", DateTime.UtcNow);
+				return;
+			}
+
+			ulong channelID = command.Channel.Id;
+			// Check if ticket exists in the database
+			if (!Database.TryGetOpenTicket(command.Channel.Id, out Database.Ticket ticket))
+			{
+				DiscordEmbed error = new DiscordEmbedBuilder
+				{
+					Color = DiscordColor.Red,
+					Description = "This channel is not a ticket."
+				};
+				await command.RespondAsync("", false, error);
+				return;
+			}
+
+			string summary = command.Message.Content.Replace(Config.prefix + "setsummary", "").Trim();
+
 			using (MySqlConnection c = Database.GetConnection())
 			{
-				// Check if the user has permission to use this command.
-				if (!Config.HasPermission(command.Member, "setsummary"))
-				{
-					DiscordEmbed error = new DiscordEmbedBuilder
-					{
-						Color = DiscordColor.Red,
-						Description = "You do not have permission to use this command."
-					};
-					await command.RespondAsync("", false, error);
-					command.Client.DebugLogger.LogMessage(LogLevel.Info, "SupportBoi", "User tried to use the setsummary command but did not have permission.", DateTime.UtcNow);
-					return;
-				}
-
-				ulong channelID = command.Channel.Id;
-				// Check if ticket exists in the database
-				if (!Database.TryGetOpenTicket(command.Channel.Id, out Database.Ticket ticket))
-				{
-					DiscordEmbed error = new DiscordEmbedBuilder
-					{
-						Color = DiscordColor.Red,
-						Description = "This channel is not a ticket."
-					};
-					await command.RespondAsync("", false, error);
-					return;
-				}
-
-				string summary = command.Message.Content.Replace(Config.prefix + "setsummary", "").Trim();
-
 				c.Open();
 				MySqlCommand update = new MySqlCommand(@"UPDATE tickets SET summary = @summary WHERE channel_id = @channel_id", c);
 				update.Parameters.AddWithValue("@summary", summary);
 				update.Parameters.AddWithValue("@channel_id", channelID);
 				update.Prepare();
 				update.ExecuteNonQuery();
+				update.Dispose();
 
 				Sheets.SetSummaryQueued(ticket.id, summary);
 
--- SupportBoi/Commands/StatusCommand.cs
+++ SupportBoi/Commands/StatusCommand.cs
@@ -14,33 +14,30 @@ namespace SupportBoi.Commands
 		[Cooldown(1, 5, CooldownBucketType.User)]
 		public async Task OnExecute(CommandContext command)
 		{
-			using (MySqlConnection c = Database.GetConnection())
+			// Check if the user has permission to use this command.
+			if (!Config.HasPermission(command.Member, "status"))
 			{
-				// Check if the user has permission to use this command.
-				if (!Config.HasPermission(command.Member, "status"))
+				DiscordEmbed error = new DiscordEmbedBuilder
 				{
-					DiscordEmbed error = new DiscordEmbedBuilder
-					{
-						Color = DiscordColor.Red,
-						Description = "You do not have permission to use this command."
-					};
-					await command.RespondAsync("", false, error);
-					command.Client.DebugLogger.LogMessage(LogLevel.Info, "SupportBoi", "User tried to use the status command but did not have permission.", DateTime.UtcNow);
-					return;
-				}
-
-				long openTickets = Database.GetNumberOfTickets();
-				long closedTickets = Database.GetNumberOfClosedTickets();
-
-				DiscordEmbed botInfo = new DiscordEmbedBuilder()
-					.WithAuthor("KarlofDuty/SupportBoi @ GitHub", "https://github.com/KarlofDuty/SupportBoi", "https://karlofduty.com/img/tardisIcon.jpg")
-					.WithTitle("Bot information")
-					.WithColor(DiscordColor.Cyan)
-					.AddField("Version:", SupportBoi.GetVersion(), false)
-					.AddField("Open tickets:", openTickets + "", true)
-					.AddField("Closed tickets (1.1.0+ tickets only):", closedTickets + " ", true);
-				await command.RespondAsync("", false, botInfo);
+					Color = DiscordColor.Red,
+					Description = "You do not have permission to use this command."
+				};
+				await command.RespondAsync("", false, error);
+				command.Client.DebugLogger.LogMessage(LogLevel.Info, "SupportBoi", "User tried to use the status command but did not have permission.", DateTime.UtcNow);
+				return;
 			}
+
+			long openTickets = Database.GetNumberOfTickets();
+			long closedTickets = Database.GetNumberOfClosedTickets();
+
+			DiscordEmbed botInfo = new DiscordEmbedBuilder()
+				.WithAuthor("KarlofDuty/SupportBoi @ GitHub", "https://github.com/KarlofDuty/SupportBoi", "https://karlofduty.com/img/tardisIcon.jpg")
+				.WithTitle("Bot information")
+				.WithColor(DiscordColor.Cyan)
+				.AddField("Version:", SupportBoi.GetVersion(), false)
+				.AddField("Open tickets:", openTickets + "", true)
+				.AddField("Closed tickets (1.1.0+ tickets only):", closedTickets + " ", true);
+			await command.RespondAsync("", false, botInfo);
 		}
 	}
 }
