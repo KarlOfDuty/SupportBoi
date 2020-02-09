e744642a63873c53d7a87ed178bc708814ec9016
Karl Essinger <xkaess22@gmail.com>
2020-02-09 23:50:13 +0100

    Added new list embed splitting

--- SupportBoi/Utilities.cs
+++ SupportBoi/Utilities.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Tyrrrz.Extensions;
+﻿using System.Collections.Generic;
 
 namespace SupportBoi
 {
@@ -12,6 +11,25 @@ namespace SupportBoi
 				return new string[0];
 			}
 			return  args.Trim().Replace("<@!", "").Replace("<@", "").Replace(">", "").Split();
+		}
+
+		public static LinkedList<string> ParseListIntoMessages(List<string> listItems)
+		{ 
+			LinkedList<string> messages = new LinkedList<string>();
+
+			foreach (string listItem in listItems)
+			{
+				if (messages.Last?.Value?.Length + listItem?.Length < 2048)
+				{
+					messages.Last.Value += listItem;
+				}
+				else
+				{
+					messages.AddLast(listItem);
+				}
+			}
+
+			return messages;
 		}
 	}
 }
